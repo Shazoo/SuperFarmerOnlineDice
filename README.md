4fbec641aa2eb3ac07b3d0a63216d9d1507ab7ba
shazoo <zhe.wu.1981@gmail.com>
2021-01-05 09:31:21 +0800

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # SuperFarmerOnlineDice
 超级农场主，在线骰子 SUPER FARMER Online Dice 
+
+
+
+## 游戏简介
+
+游戏官网：[见此](https://www.granna.pl/katalog-gier/0-wszystkie-gry/343-SUPER-FARMER-THE-CARD-GAME.html)
+
+淘宝购买链接：[领卷](https://s.click.taobao.com/HdbQVsu)
+
+玩法：[见此](https://witreader.com/articles/549625416756/)
+
+
+
+## 本程序
+
+没得说，直接丢服务器上，跑起来即可。图片都没有，利用unicode的symbols字体。
+
+![image-20210105092948434](https://img.0w0.io/u/s!AkNcBZ_Plzzpgp8gEtFxiu-jJxAIiA/image-20210105092948434.png)
+
+
+
+兼容：
+
+* 微信
+* chrome
+* edge
+* 其实应该全都支持，最简单的html+css+js，手撸的。
+
+
+
+## 评价
+
+相当不错，娃很喜欢。
+
---
+++ scripts.js
@@ -0,0 +1,22 @@
+const dice1_arr = ['🐇','🐇','🐇','🐇','🐇','🐇','🐑', '🐑', '🐖', '🐖', '🐄', '🦊'];
+const dice2_arr = ['🐇','🐇','🐇','🐇','🐇','🐇','🐑', '🐑', '🐑', '🐖', '🐎', '🐺'];
+const runbtn = document.querySelector('#run')
+
+function get_random_animal(arr) {
+  var r = Math.floor(Math.random() * arr.length);
+  return arr[r];
+}
+
+runbtn.addEventListener('click', e=> {
+  var show_ele_1 = document.querySelector('#dice1');
+  var show_ele_2 = document.querySelector('#dice2');
+  show_ele_1.innerText = get_random_animal(dice1_arr);
+  show_ele_2.innerText = get_random_animal(dice2_arr);
+  runbtn.disabled  = true;
+  runbtn.innerText = '等等再扔';
+    runbtn.innerText = '等等再扔';
+  setTimeout(function () {
+    runbtn.disabled  = false;
+    runbtn.innerText = '扔骰子';
+  }, 2000)
+})
